0d5549d6277659e40a7e4568f976da8671a114c5
Chulumanco-Mbete <5100151@mypillar7uwc.ac.za>
2023-11-28 13:48:42 +0200

    Changed the odd numbers

--- Exercise 4/Program.cs
+++ Exercise 4/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int[] nums = { 2, 4, 6, 9 };
+            int[] nums = { 2, 4, 9, 12 };
             Console.WriteLine("Original array: [{0}]", string.Join(", ", nums));
             Console.WriteLine("Does this array contain an odd number? (true/false): " + even_odd(nums));
         }
