b2f5faa87c715172e62018477b06bfdb03f944e1
Chulumanco-Mbete <5100151@mypillar7uwc.ac.za>
2023-11-28 12:01:22 +0200

    Update Commit

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversedString = Reverse(Console.ReadLine());
+            Console.WriteLine("Output: " + reversedString);
+        }
+
+        static string Reverse(string text)
+        {
+            string reverse = String.Empty;
+            for (int i = text.Length - 1; i > -1; i--)
+            {
+                reverse += text[i];
+            }
+            return reverse;
+        }
+    }
+}
